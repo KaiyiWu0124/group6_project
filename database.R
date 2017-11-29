ff2badec3438bc8da180da679fd4aee2541e0b51
Eric Song <eks20@psu.edu>
2017-11-29 11:30:29 -0500

    Added skeleton framework for database

---
+++ database.R
@@ -0,0 +1,13 @@
+#This is only for zipcode 16801 (State College)
+#If you have issues with the file not found
+#Need to use the following in console to the folder the file is in:
+#setwd("C:/Users/asdf/folder etc...")
+data=read.csv("prices.csv",header=TRUE,sep=",")
+#Column names for reference
+colnames(data)
+
+renewable=subset(data,Renewable == "Y")
+
+
+lowestprice = data[which(data==min(data$Price)),]
+
