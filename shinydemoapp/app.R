289dba8582e6f8280efad4aed7ab16f965c7b4ee
Eric Song <eks20@psu.edu>
2017-12-07 12:05:34 -0500

    Final Demo

--- shinydemoapp/app.R
+++ shinydemoapp/app.R
@@ -11,27 +11,29 @@
 library(shiny)
 library(ggplot2)
 data=read.csv("16801WestPennPower.csv",header=TRUE,sep=",")
-cf_choose = function(energytype){
-  cf_coal = 909
-  cf_naturalgas = 465
-  cf_oil = 821
-  cf_renewable = 0
-  if (energytype == "Oil"){return(cf_oil)}
-  else if (energytype =="Natural Gas"){return(cf_naturalgas)}
-  else if (energytype == "Coal"){return(cf_coal)}
-  else if (energytype == "Renewable"){return(cf_renewable)}
-}
+#Temp disabled.  Using different co2 formula
+#cf_choose = function(energytype){
+#  cf_coal = 909
+#  cf_naturalgas = 465
+#  cf_oil = 821
+#  cf_renewable = 0
+#  if (energytype == "Oil"){return(cf_oil)}
+#  else if (energytype =="Natural Gas"){return(cf_naturalgas)}
+#  else if (energytype == "Coal"){return(cf_coal)}
+#  else if (energytype == "Renewable"){return(cf_renewable)}
+#}
 #Resorts Data for plotting + converts price-per-kilowatt to price + Calculates total CO2 in kg
 sort.data = function(data,kwh,bill){
   #Reorders datatable by Price and reindexes row numbers
   data = data[order(data$Price),];rownames(data) = 1:nrow(data)
-  #Converts Price per kwh to cost
-  data$Price = data$Price *kwh
-  #Adds Column with Kg of CO2 produced with cf_choose function: (G of CO2 per KWH)/1000 * KWH
+  #Adds Column with metric tons of CO2 produced 
   for(i in 1:length(data$Price)){
     data$CO[i]=(bill/data$Price[i]) *16.44
+    #old line w/ old formula calc.
     #((cf_choose(energytype[i])/1000)*kwh)
   }
+  #Converts Price per kwh to cost
+  data$Price = data$Price *kwh
   return(data)
 } 
 
@@ -59,7 +61,7 @@ plot.costco=function(data,kwh,bill){
     #Price Bar
     geom_bar(stat="identity", aes(x=reorder(xvalue,-Price),y=Price),color="black",fill="blue")+
     #CO2 Bar
-    geom_bar(stat="identity",aes(x=xvalue,y=CO/2),color="green")+
+    geom_bar(stat="identity",aes(x=xvalue,y=CO/500),color="green")+
     #Your Bill Point
     geom_point(aes(y=bill,x=median(xvalue)),size=5,color="red")+
     #Your estminated carbon footprint
@@ -69,7 +71,7 @@ plot.costco=function(data,kwh,bill){
     #New Axis Label
     labs(title=(lowestcosttitle),subtitle=(lowestrenewtitle),x="Suppliers",y="Cost (blue)")+
     #2nd Y Axis Label/Scaling
-    scale_y_continuous(sec.axis= sec_axis(~./max(plotdata$CO),name="CO2 emissions in metric tons per year (Green)"))+
+    scale_y_continuous(sec.axis= sec_axis(~./max(plotdata$CO)*500000,name="CO2 emissions in pounds per year (Green)"))+
     annotate("text",x=median(xvalue),y=bill+5, label="Your Current Bill")
   return(plotcost)
 }
